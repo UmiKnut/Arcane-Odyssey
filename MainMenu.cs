6a1c0a7c28df8c5fed08b9e501f9f41ce0811e63
Jisuumi <92422545+UmiKnut@users.noreply.github.com>
2025-03-02 14:54:28 +0500

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No next scene in build settings.");
+        }
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ PauseMenu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false; 
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None; 
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame1()
+    {
+        Application.Quit();
+    }
+}
